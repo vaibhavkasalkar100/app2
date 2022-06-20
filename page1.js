e0de8afef74b81ceb552d472ce0c59b87f32cdc3
D_62778_Vaibhav <vaibhavkasalkar100@gmail.com>
2022-06-20 16:17:00 +0530

    file added

---
+++ page1.js
@@ -0,0 +1,39 @@
+function isOdd(number) {
+  if (number % 2 != 0) {
+    console.log(`${number} is ODD`)
+  } else {
+    console.log(`${number} is NOT ODD`)
+  }
+}
+
+isOdd(10)
+isOdd(11)
+
+function subtract(p1, p2) {
+  console.log(`${p1} + ${p2} = ${p1 - p2}`)
+}
+
+subtract(10, 4)
+function add(p1, p2) {
+  console.log(` add ${p1} + ${p2} = ${p1 + p2}`)
+}
+
+add(10, 20)
+
+function square(p1) {
+  console.log(`   square $ {p1}  = ${p1 * p1}`)
+}
+
+square(20)
+
+function cube(p1) {
+  console.log(`   cube $ {p1}  = ${p1 * p1 * p1}`)
+}
+
+cube(10)
+
+function cubes(p1) {
+  console.log(`   cube $ {p1}  = ${p1 ** 3}`)
+}
+
+cube(2)
